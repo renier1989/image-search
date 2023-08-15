aa11ba7672a56402b2dcdf79864b3548363e1c7d
Renier Vargas <renier1989@gmail.com>
2023-08-15 05:56:01 -0500

    Class97 - Header de la aplicacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Field, Form, Formik } from "formik"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="">
+      <header className="flex w-full justify-center p-5 shadow-lg relative z-10">
+        <Formik
+          initialValues={{search : ''}}
+          onSubmit={async values => 
+            // aqui se va a llamar a la API de unsplash
+            console.log(values)}
+        >
+          <Form className="flex w-full">
+            <Field name="search" 
+            className="flex w-full justify-center items-center text-center outline-none cursor-pointer text-[40px]"  
+            autoComplete="off"
+            placeholder="Type here ⌨" />
+          </Form>
+        </Formik>
+      </header>
+    </div>
   )
 }
-
-export default App
