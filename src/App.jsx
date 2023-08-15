c85d61f95ba9a692eeef639852965940f3e1ffe9
Renier Vargas <renier1989@gmail.com>
2023-08-15 07:35:01 -0500

    Class100 - Aplicando estilos al listado de imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export const App = () => {
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(false);
   const open = url => window.open(url);
-  console.log(photos);
+  
   return (
     <div className="">
       <header className="flex w-full justify-center p-5 shadow-lg relative z-10">
@@ -41,16 +41,18 @@ export const App = () => {
           </Form>
         </Formik>
       </header>
-      <div>
-        <span>{loading ? <p>Cargando...</p> : <p>nada</p> }</span>
-        <div>
+      <div className={`flex bg-[#eee] justify-center ${photos.length ? 'pb-10' : null}`}>
+        {/* <span>{loading ? <p>Cargando...</p> : <p>nada</p> }</span> */}
+        <div className="columns-3 w-[1200px] ">
           {photos.map((photo) => (
-            <article key={photo.id} onClick={()=> open(photo.links.html)}> 
-              <img src={photo.urls.regular} alt={photo.alt_description} />
-              <p>{[photo.desctiption, photo.alt_description].join(' ⚡ ')}</p>
+            <article className="inline-block mt-7 mx-7 transition-shadow ease-in-out bg-white rounded-lg hover:shadow-lg duration-500 cursor-pointer"
+            key={photo.id} onClick={()=> open(photo.links.html)}> 
+              <img className="w-[370px] rounded-t-lg " src={photo.urls.regular} alt={photo.alt_description} />
+              <p className="px-3 py-2">{[photo.desctiption, photo.alt_description].join(' ⚡ ')}</p>
             </article>
           ))}
         </div>
+
       </div>
     </div>
   );
