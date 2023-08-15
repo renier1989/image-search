d90e0c4dc782a3c7018f9c6a80adf826a9313cb9
Renier Vargas <renier1989@gmail.com>
2023-08-15 08:29:23 -0500

    mejorando la experiencia de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
 import { Field, Form, Formik } from "formik";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const App = () => {
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(false);
-  const open = url => window.open(url);
-  
+  const [currentPage, setCurrestPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [searching, setSearching] = useState("");
+
+  const open = (url) => window.open(url);
+
+  const fetchPhotos = async (searchValues, page) => {
+    // console.log(searchValues,page);
+    console.log("searchig:", searching, "searchValues", searchValues);
+    if (searching && searchValues !== searching) {
+      console.log("aaaaaa");
+      setPhotos([]);
+    }
+    setLoading(true);
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?per_page=20&page=${page}&query=${searchValues}`,
+      {
+        headers: {
+          Authorization:
+            "Client-ID D7KDpnlmqpYX4xC1hu7MNqf9jx7R61Fcb9kCar4ZGaM",
+        },
+      }
+    );
+
+    const data = await response.json();
+    setPhotos((prevPhotos) => [...prevPhotos, ...data.results]);
+    setTotalPages(data.total_pages);
+    setSearching(searchValues);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (currentPage > 1) {
+      // console.log('algo');
+      fetchPhotos(searching, currentPage);
+    }
+  }, [currentPage]);
+
+  const submitApi = async (values) => {
+    setLoading(true);
+    setCurrestPage(1);
+    fetchPhotos(values.search, 1);
+  };
+  const loadMorePhotos = () => {
+    // console.log(currentPage, totalPages)
+    if (currentPage < totalPages) {
+      setCurrestPage((prevPage) => prevPage + 1);
+    }
+  };
+
   return (
     <div className="">
       <header className="flex w-full justify-center p-5 shadow-lg relative z-10">
         <Formik
           initialValues={{ search: "" }}
-          onSubmit={async (values) => {
-            setLoading(true);
-             
-              
-              const response = await fetch(
-                `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
-                {
-                  headers: {
-                    Authorization:
-                      "Client-ID D7KDpnlmqpYX4xC1hu7MNqf9jx7R61Fcb9kCar4ZGaM",
-                  },
-                }
-              );
-
-              const data = await response.json();
-              setPhotos(data.results);            
-            // console.log(data);
-            setLoading(false);
-          }}
+          onSubmit={async (values) => submitApi(values)}
         >
           <Form className="flex w-full">
             <Field
@@ -41,19 +71,36 @@ export const App = () => {
           </Form>
         </Formik>
       </header>
-      <div className={`flex bg-[#eee] justify-center ${photos.length ? 'pb-10' : null}`}>
+      <div
+        className={`flex bg-[#eee] justify-center ${
+          photos.length ? "pb-10" : null
+        }`}
+      >
         {/* <span>{loading ? <p>Cargando...</p> : <p>nada</p> }</span> */}
         <div className="columns-3 w-[1200px] ">
           {photos.map((photo) => (
-            <article className="inline-block mt-7 mx-7 transition-shadow ease-in-out bg-white rounded-lg hover:shadow-lg duration-500 cursor-pointer"
-            key={photo.id} onClick={()=> open(photo.links.html)}> 
-              <img className="w-[370px] rounded-t-lg " src={photo.urls.regular} alt={photo.alt_description} />
-              <p className="px-3 py-2">{[photo.desctiption, photo.alt_description].join(' ⚡ ')}</p>
+            <article
+              className="inline-block mt-7 mx-7 transition-shadow ease-in-out bg-white rounded-lg hover:shadow-lg duration-500 cursor-pointer"
+              key={photo.id}
+              onClick={() => open(photo.links.html)}
+            >
+              <img
+                className="w-[370px] rounded-t-lg "
+                src={photo.urls.regular}
+                alt={photo.alt_description}
+              />
+              <p className="px-3 py-2">
+                {[photo.desctiption, photo.alt_description].join(" ⚡ ")}
+              </p>
             </article>
           ))}
         </div>
-
       </div>
+        {photos.length && currentPage < totalPages ? (
+          <div className="flex bg-[#eee] justify-center pb-3">
+            <button className="px-4 py-2 bg-slate-400 rounded-md font-semibold text-white" onClick={loadMorePhotos}>ver mas</button>
+          </div>
+        ) : null}
     </div>
   );
 };
