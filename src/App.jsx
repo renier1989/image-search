1721124b8495c00f67ed5b7e69abab69fe735d96
Renier Vargas <renier1989@gmail.com>
2023-08-15 06:31:12 -0500

    Class99 - iterando imagenes.

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,31 @@ import { useState } from "react";
 
 export const App = () => {
   const [photos, setPhotos] = useState([]);
-  console.log(photos)
+  const [loading, setLoading] = useState(false);
+  console.log(photos);
   return (
     <div className="">
       <header className="flex w-full justify-center p-5 shadow-lg relative z-10">
         <Formik
           initialValues={{ search: "" }}
           onSubmit={async (values) => {
-            const response = await fetch(
-              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
-              {
-                headers: {
-                  Authorization:
-                    "Client-ID D7KDpnlmqpYX4xC1hu7MNqf9jx7R61Fcb9kCar4ZGaM",
-                },
-              }
-            );
-            const data = await response.json();
+            setLoading(true);
+             
+              
+              const response = await fetch(
+                `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+                {
+                  headers: {
+                    Authorization:
+                      "Client-ID D7KDpnlmqpYX4xC1hu7MNqf9jx7R61Fcb9kCar4ZGaM",
+                  },
+                }
+              );
+
+              const data = await response.json();
+              setPhotos(data.results);            
             // console.log(data);
-            setPhotos(data.results);
+            setLoading(false);
           }}
         >
           <Form className="flex w-full">
@@ -34,6 +40,17 @@ export const App = () => {
           </Form>
         </Formik>
       </header>
+      <div>
+        <span>{loading ? <p>Cargando...</p> : <p>nada</p> }</span>
+        <div>
+          {photos.map((photo) => (
+            <article key={photo.id}> 
+              <img src={photo.urls.regular} alt={photo.alt_description} />
+              <p>{[photo.desctiption, photo.alt_description].join(' ⚡ ')}</p>
+            </article>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
